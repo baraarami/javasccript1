3e8ec6732ab9c860d0abe77d22f51b157b972dfa
baraarami <baraajohmani20@gmail.com>
2021-02-23 15:21:53 +0200

    create a web page with js

---
+++ js/app.js
@@ -0,0 +1,44 @@
+
+
+confirm('welcome to this our Humble site !')
+
+var age=22;
+var name='baraa';
+var ispassed=true;
+var isabsent=false;
+var username="bara'a";
+
+if (age<40){
+     alert("you are below the legal age !");
+    console.log(" first time !");
+}
+
+if (age>=19){
+    alert("welcome to our site !");
+    console.log(" second time !");
+}
+
+var username=prompt("please provide your name ?");
+var char1=prompt('first name');
+var char2=prompt('second name');
+
+if (char1===char2){
+    console.log("similler movie");
+} else{
+    console.log("another one ");
+}
+console.log(username);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
