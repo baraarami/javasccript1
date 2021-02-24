48a80069b558c51a7ef0335a0ad416239f3c5f42
baraarami <baraajohmani20@gmail.com>
2021-02-24 16:48:57 +0200

    added functions

--- js/app.js
+++ js/app.js
@@ -10,19 +10,27 @@ var username="bara'a";
 
 if (age<40){
      alert("you are below the legal age !");
-     
+
     console.log(" first time !");
 }
 
-if (age>=19){
-    alert("welcome to our site !");
+
+if (age>19){
     console.log(" second time !");
 }
 
-var username=prompt("please provide your name ?");
-var char1=prompt('first name');
-var char2=prompt('second name');
+// var username=prompt("please provide your name ?");
+//var char1=prompt('first name');
+//var char2=prompt('second name');
 
+function userName(firstName , secondName , thirdName)
+{
+    prompt("please enter your first name ");
+    prompt("please enter your second name ");
+}
+userName();
+var char1=0;
+var char2=0;
 if (char1===char2){
     console.log("similler movie");
 } else{
@@ -30,6 +38,18 @@ if (char1===char2){
 }
 console.log(username);
 
+//function showEmage (){
+  //  return document.getElementById("import");
+//}
+//showEmage();
+
+function showEmage (){
+    for(i = 0; i < 4 ; i++){
+
+        document.write('<img src="http://radicalhub.com/wp-content/uploads/2018/07/javascript.jpg"/> ')
+    }
+}
+showEmage();
 
 
 
