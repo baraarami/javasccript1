7229d4767e20cc394744a45812de683cb708d74e
baraarami <baraajohmani20@gmail.com>
2021-02-25 10:00:59 +0200

    correct things

--- js/app.js
+++ js/app.js
@@ -10,17 +10,31 @@ var username="bara'a";
 
 if (age<40){
      alert("you are below the legal age !");
+
     console.log(" first time !");
 }
 
-if (age>=19){
-    alert("welcome to our site !");
+
+if (age>19){
     console.log(" second time !");
 }
 
-var username=prompt("please provide your name ?");
-var char1=prompt('first name');
-var char2=prompt('second name');
+// var username=prompt("please provide your name ?");
+//var char1=prompt('first name');
+//var char2=prompt('second name');
+
+function userName(firstName , secondName , thirdName)
+{
+    prompt("please enter your first name ");
+    prompt("please enter your second name ");
+
+}
+userName();
+
+var Number= prompt("Please Enter a number of photo you wont to see of javascript !");
+
+var char1=0;
+var char2=0;
 
 if (char1===char2){
     console.log("similler movie");
@@ -28,6 +42,23 @@ if (char1===char2){
     console.log("another one ");
 }
 console.log(username);
+ 
+var enter=prompt("Do you love js ?");
+var yes='yes';
+var no='no';
+
+while (enter != yes ){
+    enter=prompt("Are you shour !!");
+}
+while (enter != yes){
+    enter=prompt("you have you're chance !");
+}
+for (var counter=1 ; counter < Number && counter < 10 ; counter++) {
+
+    document.write('<img src="http://radicalhub.com/wp-content/uploads/2018/07/javascript.jpg" />');
+    document.write(counter);
+
+}
 
 
 
